50dcf6f66681ec426bac1051d823d358dfa9989a
Marvin Xu <marvinxu99@hotmail.com>
2020-03-12 23:36:05 -0700

    ..

--- web_project/settings.py
+++ web_project/settings.py
@@ -187,6 +187,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
+# addin the following will cause HEROU rejected to deploy????
+# but works locally???heroku
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 #     os.path.join(BASE_DIR, "generated_codes"),
