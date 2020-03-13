5f2fef858e02c9142e5abe4613e4c741dcbfb155
Marvin Xu <marvinxu99@hotmail.com>
2020-03-12 23:30:40 -0700

    ..

--- web_project/settings.py
+++ web_project/settings.py
@@ -187,9 +187,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     os.path.join(BASE_DIR, "generated_codes"),
+# ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "generated_codes"),
+    "/static/",
+    "/generated_codes/",
 ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
