ecaa3aeaadd60d8f1226f9c3a80e456c46535047
Marvin Xu <marvinxu99@hotmail.com>
2020-03-14 16:00:16 -0700

    change barcode.py to barcode_i.py

--- polls/views/__init__.py
+++ polls/views/__init__.py
@@ -2,4 +2,4 @@ from .emails import email, email_contact
 # from .views import QuestionView, detail, ResultsView, vote, about, \
 #             contact, email, index, winter_winnpy, winter_univer, face_recognition
 from .views import *        
-from .barcode import barcode_req, barcode_disp 
+from .barcode_i import barcode_req, barcode_disp 
