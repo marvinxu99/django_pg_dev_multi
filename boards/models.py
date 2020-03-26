4c3d8e1b88b061099d9d96fdc3451e764fb1ce30
Marvin Xu <marvinxu99@hotmail.com>
2020-03-26 01:14:42 -0700

    change the length board name from 30 t0 60 chars

--- boards/models.py
+++ boards/models.py
@@ -7,7 +7,7 @@ import math
 
 
 class Board(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=60, unique=True)
     description = models.CharField(max_length=100)
 
     def __str__(self):
