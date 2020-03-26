54028038eed073b884e4f5aabc63f64ae7b63f34
Marvin Xu <marvinxu99@hotmail.com>
2020-03-25 19:28:02 -0700

    ...

--- web_project/settings.py
+++ web_project/settings.py
@@ -169,8 +169,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'   # http://whitenoise.evans.io/e#n/stable/django.html
 # http://whitenoise.evans.io/en/stable/django.html#storage-troubleshoot
