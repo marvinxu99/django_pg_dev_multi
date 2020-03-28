edfb79fe668cf3174b4189e966129f27dfad77b5
Marvin Xu <marvinxu99@hotmail.com>
2020-03-27 20:58:26 -0700

    barcode is to be generated frontend

--- polls/views/barcodes_js.py
+++ polls/views/barcodes_js.py
@@ -4,14 +4,14 @@ from django.shortcuts import render
 
 def barcode_gen_js(request):
     '''
-    Generate barcode at client side using jQuery
+    Generate barcode at client side using jQuery and jquery-barcode.js
     '''
-    try:
-        if request.GET['code'] == '1d':
-            bc_type = '1d'
-        else:
-            bc_type = '2d'
-    except: 
-        bc_type = '2d'
+    # try:
+    #     if request.GET['code'] == '1d':
+    #         bc_type = '1d'
+    #     else:
+    #         bc_type = '2d'
+    # except: 
+    #     bc_type = '2d'
 
     return render(request, 'polls/barcode_gen_js.html', { 'bc_type': bc_type})
