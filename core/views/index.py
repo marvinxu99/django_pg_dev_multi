06eb5d5ea364a8a82979ca26ea563541a2a830ab
Marvin Xu <marvinxu99@hotmail.com>
2020-05-03 22:48:29 -0700

    ...

--- core/views/index.py
+++ core/views/index.py
@@ -8,7 +8,7 @@ def winter_winnpy(request):
     return redirect('http://winterai.herokuapp.com')
 
 def winter_univer(request):
-    return redirect('http://winnpysoft.pythonanywhere.com')
+    return redirect('http://winnpy.pythonanywhere.com')
 
 def face_recognition(request):
     return redirect('http://winter-x-face.herokuapp.com')
