28e11fbd7d92bdef43a0905b2bfac495e811211f
Marvin Xu <marvinxu99@hotmail.com>
2020-06-19 11:49:32 -0700

    added Unit of Measure(UOM)

--- core/constants.py
+++ core/constants.py
@@ -1,10 +1,67 @@
-STATE_1 = 0
-STATE_2 = 1
-STATE_3 = 2
+from django.db import models
+import calendar
+from django.utils.translation import gettext_lazy as _
 
-STATE_CHOICES = (
-    (STATE_1, 'STATE 1'),
-    (STATE_2, 'STATE 2'),
-    (STATE_3, 'STATE 3')
-)
+
+
+class ACTIVE_STATUS(models.TextChoices):
+    ACTIVE = '01', _("Active")
+    COMBINED = '02', _('Combined') 
+    COMBINEHIST = '03', _('Historical value - combined')
+    DELETED = '04', _('Deleted')
+    DRAFT = '05', _('Draft')
+    INACTIVE = '06', _('Inactive')
+    RECALL = '07', _('Recall')
+    REVIEW = '08', _('Review')
+    SUSPENDED = '09', _('Suspended')
+    UNKNOWN = '10', _('Unknown')
+
+# Unit of Measure(UOM) - all
+class UOM(models.TextChoices):
+    DEGC = 'C', _("degree Celsius")
+    DEGC = 'F', _("degree Fahrenheit")
+
+# Unit of Measure(UOM) - temperature
+class UOM_TEMP(models.TextChoices):
+    DEGC = 'C', _("degree Celsius")
+    DEGC = 'F', _("degree Fahrenheit")
+
+# Loan Status
+class LOAN_STATUS(models.TextChoices):
+    AVAILABLE = 'a', _('Available')
+    MAINTENANCE = 'm', _('Maintenance')
+    ON_LOAN = 'o', _('On loan')
+    RESERVED = 'r', _('Reserved')
+
+
+#MONTH_CHOICES = [(str(i), calendar.month_name[i]) for i in range(1,13)]
+class MONTH_CHOICES(models.TextChoices):
+    JAN = '1', _("January")
+    FEB = '2', _("Febuary")
+    MAR = '3', _("March")
+    APR = '4', _('April')
+    MAY = '5', _('May')
+    JUN = '6', _('June')
+    JUL = '7', _('July')
+    AUG = '8', _('August')
+    SEP = '9', _('September')
+    OCT = '10', _('October')
+    NOV = '11', _('November')
+    DEC = '12', _('December')
+
+
+MEDIA_CHOICES = [
+    ('Audio', (
+            ('vinyl', 'Vinyl'),
+            ('cd', 'CD'),
+        )
+    ),
+    ('Video', (
+            ('vhs', 'VHS Tape'),
+            ('dvd', 'DVD'),
+        )
+    ),
+    ('unknown', 'Unknown'),
+]
+
 
--- core/models/item_defintion.py
+++ core/models/item_defintion.py
@@ -8,18 +8,37 @@ class ItemDefinition(models.Model):
     item_id = models.BigAutoField(primary_key=True, editable=False)
 
     active_ind = models.BooleanField("Active", default=True)
-
-    item_type_cd = models.BigIntegerField(null=True, blank=True)
-    reusable_ind = models.BooleanField("Reusable", default=False)
-   
     active_status_cd = models.BigIntegerField(null=True, blank=True)
     active_status_dt_tm = models.DateTimeField(null=True, blank=True)
     active_status_prsnl_id = models.BigIntegerField(null=True, blank=True)
 
+    approved_ind = models.BooleanField(default=false)
+
+    item_type_cd = models.BigIntegerField(null=True, blank=True)
+    reusable_ind = models.BooleanField("Reusable", default=False)
+   
+    batch_qty = models.BigIntegerField(null=True, blank=True)
+    chargeable_ind = odels.BooleanField(default=True)
+    component_ind = models.BooleanField(default=True)
+    component_usage_ind = models.BooleanField(default=False)
+
+    create_appctx = models.BigIntegerField(null=True, blank=True)
     create_dt_tm = models.DateTimeField(auto_now_add=True)
     create_id = models.BigIntegerField(null=True, blank=True)
     create_task = models.BigIntegerField(null=True, blank=True)
-    create_appctx = models.BigIntegerField(null=True, blank=True)
+
+    inst_id = models.BigIntegerField(null=True, blank=True)
+
+    item_level_flag = models.IntegerField(null=True, blank=True)
+    latex_ind = models.BooleanField(default=False)
+
+    temp_store_max = models.IntegerField(null=True, blank=True)
+    temp_store_min = models.IntegerField(null=True, blank=True)
+    temp_uom_cd = 
+
+
+    shelf_life = models.IntegerField(null=True, blank=True)
+    shelf_life_uom_cd = models.IntegerField('Shelf Life UOM', null=True, blank=True)
 
     updt_cnt = models.IntegerField(null=True, blank=True)
     updt_dt_tm = models.DateTimeField(null=True, blank=True)  
@@ -27,15 +46,8 @@ class ItemDefinition(models.Model):
     updt_task = models.BigIntegerField(null=True, blank=True)
     updt_appctx = models.BigIntegerField(null=True, blank=True)
     
-    component_ind = models.BooleanField(default=True)
     
-    shelf_life = models.IntegerField(null=True, blank=True)
-    shelf_life_uom_cd = models.IntegerField('Shelf Life UOM', null=True, blank=True)
     
-    component_usage_ind = models.BooleanField(default=False)
-    latex_ind = models.BooleanField(default=False)
-    item_level_flag = models.IntegerField(null=True, blank=True)
-    chargeable_ind = models.BooleanField(default=True)
 
 
     class Meta:
