8e4cb910f9a347d77cac2118d2c1df8637ea0dad
Marvin Xu <marvinxu99@hotmail.com>
2020-06-30 14:41:53 -0700

    post_save signal

--- core/signals.py
+++ core/signals.py
@@ -5,9 +5,10 @@ from django.dispatch import receiver
 from .models import TransEvent
 
 
+# update the EVENT_ID to be the trans_event_id when the row is firs created
 @receiver(post_save, sender=TransEvent)
 def set_event_id(sender, instance, **kwargs):
-    print("post_save TransEvent.")
+    # print("post_save TransEvent.")
     if not instance.event_id: 
         instance.event_id = instance.trans_event_id
         instance.save()
