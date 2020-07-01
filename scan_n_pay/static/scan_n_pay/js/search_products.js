7ca44bc19c3da1d2a9f7fc152801d60f4548d4fa
Marvin Xu <marvinxu99@hotmail.com>
2020-06-30 19:40:45 -0700

    added modal dialog to display searched product

---
+++ scan_n_pay/static/scan_n_pay/js/search_products.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#searchProductsModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var data = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        //var modal = $(this)
+        //modal.find('.modal-title').text('TEST:' + recipient)
+        //$("#detail_img").attr("src", data)
+        
+
+    });
+  
+    $('#confirmDeleteModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var data = button.data('whatever') // Extract info from data-* attributes
+        $("#confirm_delete_post").attr("href", data)
+    });
+  
+  });
