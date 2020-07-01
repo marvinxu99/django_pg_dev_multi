59c61f9e9ef50c058ca7fe5e1b3545ed3494d0a4
Marvin Xu <marvinxu99@hotmail.com>
2020-06-30 19:55:49 -0700

    added some tooltips

--- scan_n_pay/static/scan_n_pay/js/search_products.js
+++ scan_n_pay/static/scan_n_pay/js/search_products.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+
     $('#searchProductsModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
         var data = button.data('whatever') // Extract info from data-* attributes
@@ -7,7 +9,7 @@ $(document).ready(function(){
         //var modal = $(this)
         //modal.find('.modal-title').text('TEST:' + recipient)
         //$("#detail_img").attr("src", data)
-        
+
 
     });
   
