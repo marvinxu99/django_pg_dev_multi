9afff8c3c9a431de9c349f298b05a2238af63bfa
Marvin Xu <marvinxu99@hotmail.com>
2020-07-02 19:06:14 -0700

    added book_create function via AJAX

--- books/static/books/js/books.js
+++ books/static/books/js/books.js
@@ -14,7 +14,10 @@ $(function () {
     });
 
     $("#modal-book").on("submit", ".js-book-create-form", function () {
+        // In this context, this refers to the element with class .js-book-create-form. 
+        // Which is the element that fired the submit event. So when we select $(this) we are selecting the actual form.
         var form = $(this);
+
         $.ajax({
             url: form.attr("action"),
             data: form.serialize(),
@@ -22,13 +25,18 @@ $(function () {
             dataType: 'json',
             success: function (data) {
                 if (data.form_is_valid) {
-                    alert("Book created!");  // <-- This is just a placeholder for now for testing
+                    $("#book-table tbody").html(data.html_book_list);  // <-- Replace the table body
+                    $("#modal-book").modal("hide");  // <-- Close the modal dialogue
                 }
                 else {
                     $("#modal-book .modal-content").html(data.html_form);
                 }
             }
         });
+
+        //A very important detail here: in the end of the function we are returning false. That’s because 
+        // we are capturing the form submission event. So to avoid the browser to perform a full HTTP POST 
+        // to the server, we cancel the default behavior returning false in the function
         return false;
     });
   
--- books/views.py
+++ books/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import datetime
 
 from .models import Book
 from .forms import BookForm
 
-
+# List all the books created.
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.objects.all()    
     return render(request, 'books/book_list.html', {'books': books})
 
+
+# Create a new book
 def book_create(request):
     data = dict()
 
     print("ajax call recieved.")
-    
+
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
+            
             data['form_is_valid'] = True
+            
+            books = Book.objects.all()
+            data['html_book_list'] = render_to_string('books/includes/partial_book_list.html', {
+                'books': books
+            })
+            
         else:
             data['form_is_valid'] = False
     else:
+        # form = BookForm()
         form = BookForm(initial={ 'publication_date': datetime.date.today() } )
 
     context = {'form': form}
-    html_form = render_to_string('books/includes/partial_book_create.html',
+    data['html_form'] = render_to_string('books/includes/partial_book_create.html',
         context,
         request=request,
     )
-    return JsonResponse({'html_form': html_form})
+     
+    return JsonResponse(data)
     
