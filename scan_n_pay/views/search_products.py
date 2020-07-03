80073d71d40293f69d3138db767e5b3a9f627bd2
Marvin Xu <marvinxu99@hotmail.com>
2020-07-03 10:38:32 -0700

    added search products queries

--- scan_n_pay/views/search_products.py
+++ scan_n_pay/views/search_products.py
@@ -12,6 +12,20 @@ from core.constants import ITEM_TYPE
 
 
 def search_products(request):
+
+    data = {
+        'itemsCount': 0,
+        'items': [
+            # {
+            #     'itemId': -1,
+            #     'itemIdentId': -1,
+            #     'itemPriceId': -1,
+            #     'description': '',
+            #     'price': -0.01,
+            # },
+        ],
+    }
+
     # item types to search for
     search_item_types = [
         ITEM_TYPE.GENERAL,
@@ -21,36 +35,42 @@ def search_products(request):
         
     # Item
     qs_items = Item.objects.filter(active_ind=True, item_type_cd__in=search_item_types)
-    print(qs_items)
-    items_count = len(qs_items)
-    print(f'item_count = {items_count}')
+    data['itemsCount'] = len(qs_items)
+    print(f"item_count = {data['itemsCount']}")
 
-    # ItemIdentifier
-    qs_identifier = qs_items[0].Identifiers.filter(
-            active_ind = True,
-            item_identifier_type_cd = ITEM_IDENTIFIER_TYPE.DESCRIPTION
-        )[0]
-    item_identifier_id = qs_identifier.item_identifier_id 
-    print(f'item identifier id = {item_identifier_id}')
+    # # ItemIdentifier
+    # qs_identifier = qs_items[0].Identifiers.filter(
+    #         active_ind = True,
+    #         item_identifier_type_cd = ITEM_IDENTIFIER_TYPE.DESCRIPTION
+    #     )[0]
+    # item_identifier_id = qs_identifier.item_identifier_id 
+    # print(f'item identifier id = {item_identifier_id}')
 
-    # ItemPrice
-    qs_price = qs_items[0].Prices.filter(price_type_cd = ITEM_PRICE_TYPE.QUOTE)[0]
-    item_price_id = qs_price.item_price_id
-    price = qs_price.price
-    print(f'item_price_id = {item_price_id }, price = {price}')
+    # # ItemPrice
+    # qs_price = qs_items[0].Prices.filter(price_type_cd = ITEM_PRICE_TYPE.QUOTE)[0]
+    # item_price_id = qs_price.item_price_id
+    # price = qs_price.price
+    # print(f'item_price_id = {item_price_id }, price = {price}')
 
+    for item in qs_items: 
+        item_details = {}
 
-    data = {
-        'itemsCount': 0,
-        'items': [
-            {
-                'itemId': -1,
-                'itemIdentId': -1,
-                'itemPriceId': -1,
-                'description': '',
-                'price': -0.01,
-            },
-        ],
-    }
+        # ItemIdentifier
+        qs_identifier = item.Identifiers.filter(
+                active_ind = True,
+                item_identifier_type_cd = ITEM_IDENTIFIER_TYPE.DESCRIPTION
+            )[0]
+        item_details['itemIdentId'] = qs_identifier.item_identifier_id 
+        print(f"item identifier id = {item_details['itemIdentId']}")
+
+        # ItemPrice
+        qs_price = item.Prices.filter(price_type_cd = ITEM_PRICE_TYPE.QUOTE)[0]
+        item_details['itemPriceId'] = qs_price.item_price_id
+        item_details['price'] = qs_price.price
+        print(f"item_price_id = {item_details['itemPriceId'] }, price = {item_details['price']}")
+
+        data['items'].append(item_details)
+
+    print(data)
 
     return JsonResponse(data)
