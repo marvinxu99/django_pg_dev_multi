3f49bcb213ea11a4b57b80feb2abd02232db6b66
Marvin Xu <marvinxu99@hotmail.com>
2020-07-13 18:05:26 -0700

    changed google smtp to EMAIL_USE_SSL=True

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,44 +6,40 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-
     path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('my_account', views.UserUpdateView.as_view(), name='my_account'),
-
+    
+    # Django login, logout, and password management
+    # https://docs.djangoproject.com/en/3.0/topics/auth/default/#all-authentication-views
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+            template_name='accounts/password_change.html', 
+            success_url=reverse_lazy('accounts:password_change_done')
+        ), name='password_change'),
+    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(
+            template_name='accounts/password_change_done.html'
+        ), name='password_change_done'),
+    
     # Password reset through email
-    path('reset/',
+    path('password_reset/',
         auth_views.PasswordResetView.as_view(
             template_name='accounts/password_reset.html',
             email_template_name='accounts/password_reset_email.html',
             subject_template_name='accounts/password_reset_subject.txt',
             success_url=reverse_lazy('accounts:password_reset_done')
-        ),
-        name='password_reset'), 
-    path('reset/done/',
+        ), name='password_reset'), 
+    path('password_reset/done/',
         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
         name='password_reset_done'),
-    # !!! Ensure LOGIN_URL='accounts:login' has been set in settings.py #
-    path('reset/complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
-        name='password_reset_complete'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('reset/<uidb64>/<token>/',
         auth_views.PasswordResetConfirmView.as_view(
             template_name='accounts/password_reset_confirm.html',
             success_url=reverse_lazy('accounts:password_reset_complete')
         ),
         name='password_reset_confirm'),
-
-    # Password change
-    path('settings/password/done/', 
-        auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
-        name='password_change_done'),
-    path('settings/password/', 
-        auth_views.PasswordChangeView.as_view(
-            template_name='accounts/password_change.html', 
-            success_url=reverse_lazy('accounts:password_change_done')
-        ),
-        name='password_change'),
+    path('reset/done/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'),        
 
 ]
--- main_project/settings.py
+++ main_project/settings.py
@@ -105,10 +105,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('MY_GMAIL')
 EMAIL_HOST_PASSWORD = config('MY_GMAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-# EMAIL_USE_SSL = True
-# EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
