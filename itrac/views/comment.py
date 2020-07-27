83a39f15e6b751257cba586f423a4f001a4b6d40
Marvin Xu <marvinxu99@hotmail.com>
2020-07-26 20:50:53 -0700

    added comment delete

--- itrac/views/comment.py
+++ itrac/views/comment.py
@@ -10,14 +10,36 @@ from ..forms import CommentForm
 
 
 @login_required()
-def create_or_edit_comment(request, issue_pk, pk=None):
+def create_comment(request, issue_pk):
     """
     Create a view that allows us to create
     or edit a comment depending if the Comment ID
     is null or not
     """
     issue = get_object_or_404(Issue, pk=issue_pk)
-    comment = get_object_or_404(Comment, pk=pk) if pk else None
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.instance.issue = issue
+            form.save()
+            # notify.send(request.user, recipient=issue.author, verb="added a comment to your Issue: " + issue.title)
+            # messages.success(request, 'Comment Saved!')
+            return redirect('itrac:issue_detail', issue_pk)
+    else:
+        form = CommentForm()
+        
+    return render(request, 'itrac/comment_create.html', {'form': form})
+
+@login_required()
+def edit_comment(request, issue_pk, pk):
+    """
+    Create a view that allows us to create
+    or edit a comment depending if the Comment ID
+    is null or not
+    """
+    issue = get_object_or_404(Issue, pk=issue_pk)
+    comment = get_object_or_404(Comment, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST, request.FILES, instance=comment)
         if form.is_valid():
@@ -30,8 +52,10 @@ def create_or_edit_comment(request, issue_pk, pk=None):
     else:
         form = CommentForm(instance=comment)
         
-    return render(request, 'itrac/commentform.html', {'form': form})
+    return render(request, 'itrac/comment_edit.html', {'form': form})
 
 @login_required()
 def delete_comment(request, issue_pk, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
     return redirect('itrac:issue_detail', issue_pk)
