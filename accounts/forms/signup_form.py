7036b2e1cacc1759493be075bbf1d81163f7ab1d
Marvin Xu <marvinxu99@hotmail.com>
2020-07-30 21:14:02 -0700

    removed email field from signup form; added resizable splitter window

--- accounts/forms/signup_form.py
+++ accounts/forms/signup_form.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from ..models import User as CustomUser
 
 class SignUpForm(UserCreationForm):
-    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    #email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2')
+        #fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'password1', 'password2')
