8d5291b1389ded894a5482808a0b76dcadde8085
Marvin Xu <marvinxu99@hotmail.com>
2020-08-12 07:51:43 -0700

    minor style edit

--- itrac/views/itrac_utils.py
+++ itrac/views/itrac_utils.py
@@ -1,6 +1,7 @@
 import string 
 from django.utils.text import slugify 
 import random
+
   
 def random_string_generator(size = 10, chars = string.ascii_lowercase + string.digits): 
     return ''.join(random.choice(chars) for _ in range(size)) 
