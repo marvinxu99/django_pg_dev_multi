c6587275abc0572affafee03c7294f086aea13ff
Marvin Xu <marvinxu99@hotmail.com>
2020-12-19 17:09:30 -0800

    added auto export files from dev to heroku and winn

--- ls_files_updated.py
+++ ls_files_updated.py
@@ -1,7 +1,51 @@
 import os, time
+from pathlib import Path
 
-_dir = os.getcwd()
-files = (fle for rt, _, f in os.walk(_dir) for fle in f if (time.time() - os.stat(
-    os.path.join(rt, fle)).st_mtime) < 300)
+# Define look back time
+#LOOKBACK_TIME = 300    # 5 mins
+LOOKBACK_TIME = 3600    # 60 mins
 
-print(list(files))
+# Define the folders to be excluded
+DIRS_TO_EXCLUDE = ['venv']
+
+PATH_SOURCE = os.getcwd()
+PATH_TARGET1 = Path(PATH_SOURCE).parent.joinpath('django_pg_heroku')
+PATH_TARGET2 = os.path.join(Path(PATH_SOURCE).parent, 'django_pg_winn')
+
+
+sdirs_to_exclude = list(map(lambda dir: os.path.join(PATH_SOURCE, dir), DIRS_TO_EXCLUDE))
+
+
+
+source_files = []
+
+# files = (fle for rt, _, f in os.walk(_dir) for fle in f if (time.time() - os.stat(
+#     os.path.join(rt, fle)).st_mtime) < LOOKBACK_TIME)
+
+# Based on the solution from StackOverflow.com
+# https://stackoverflow.com/questions/18394147/
+def run_fast_scandir(dir, ext):    # dir: str, ext: list
+    subfolders, files = [], []
+
+    for f in os.scandir(dir):
+        if f.is_dir() and (f not in sdirs_to_exclude):
+            subfolders.append(f.path)
+        if f.is_file():
+            #if os.path.splitext(f.name)[1].lower() in ext:
+            if (time.time() - os.stat(f).st_mtime) < LOOKBACK_TIME:
+                files.append(f.path)
+
+    for dir in list(subfolders):
+        sf, f = run_fast_scandir(dir, ext)
+        subfolders.extend(sf)
+        files.extend(f)
+
+    return subfolders, files
+
+source_folders, source_files = run_fast_scandir(PATH_SOURCE, [".txt"])
+
+print(list(source_files))
+
+print(sdirs_to_exclude)
+print(PATH_TARGET1)
+print(PATH_TARGET2)
