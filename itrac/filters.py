6a805eff7d87ce91a49ef57a7444ebdb3be41bb8
Marvin Xu <marvinxu99@hotmail.com>
2021-01-17 18:26:22 -0800

    Separated tags from projects

--- itrac/filters.py
+++ itrac/filters.py
@@ -1,12 +1,16 @@
 from .models import Issue
 import django_filters
 
+'''
+    django-filters:
+    https://django-filter.readthedocs.io/en/stable/guide/usage.html
+'''
 class IssueFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Issue
-        fields = ['issue_type', 'title', 'status', 'priority', 'is_resolved', 'tags', 'author', 'assignee']
+        fields = ['issue_type', 'title', 'status', 'priority', 'is_resolved', 'author', 'assignee']
 
 
 # Super ursers can view all projects
@@ -15,4 +19,4 @@ class IssueFilter_superuser(django_filters.FilterSet):
 
     class Meta:
         model = Issue
-        fields = ['project', 'issue_type', 'title', 'status', 'priority', 'is_resolved', 'tags', 'author', 'assignee']
+        fields = ['project', 'issue_type', 'title', 'status', 'priority', 'is_resolved', 'author', 'assignee']
