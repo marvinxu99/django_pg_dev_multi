9ab02841b90614f51a3c55a95febb654241ebd98
Marvin Xu <marvinxu99@hotmail.com>
2021-01-25 19:18:43 -0800

    added watcher model

---
+++ itrac/models/issue_watcher.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+
+from .issue import Issue
+
+
+class IssueWatcher(models.Model):
+    """
+    Watcher
+    """
+    watcher = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='+', on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, related_name='watchers', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "itrac_issue_watcher"
+        verbose_name = "issue_watcher"
+        verbose_name_plural = "issue_watchers"
+        ordering = ['watcher']
+
+    def __str__(self):
+        return f'{issue}: {watcher}'
