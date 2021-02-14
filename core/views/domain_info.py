2acc95277d8216f2d46a648b8341095412c2033c
Marvin Xu <marvinxu99@hotmail.com>
2021-02-14 10:40:41 -0800

    optimized dm_info display

--- core/views/domain_info.py
+++ core/views/domain_info.py
@@ -1,13 +1,19 @@
-from django.conf import settings
 from django.shortcuts import render
+from django.conf import settings
 
 
 def domain_info(request):
+    dm_info = []
+
+    dm_info.append(("DOMAIN", settings.DOMAIN))
+    dm_info.append(("PROD_DEPLOY", settings.PROD_DEPLOY))
+    dm_info.append(("DEBUG", settings.DEBUG))
+    dm_info.append(("ADMINS", settings.ADMINS))
+    dm_info.append(("DB_HOST", settings.DB_HOST))
+    dm_info.append(("DB_NAME", settings.DB_NAME))
+
     context = {
-        "DOMAIN": settings.DOMAIN,
-        "PROD_DEPLOY": settings.PROD_DEPLOY,
-        "DEBUG": settings.DEBUG,
-        "ADMINS": settings.ADMINS,
+        "dm_info": dm_info
     }
 
     return render(request, 'core/domain_info/domain_info.html', context)
